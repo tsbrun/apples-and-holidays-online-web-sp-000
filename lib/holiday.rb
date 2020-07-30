5836e87c98e4dcc40e7a852ccc705f50db6e2f13
Anya Brun <tsbrun@icloud.com>
2020-07-30 17:22:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,9 +38,6 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
   holidays_with_bbqs = []
 
   holiday_hash.each do |season, holiday_array|
