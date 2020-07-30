d1a73a159e421012013d3c088a2feaa98b94b23a
Anya Brun <tsbrun@icloud.com>
2020-07-30 17:21:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,13 +29,6 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
   holiday_hash.each do |season, holiday_array|
     puts "#{season.to_s.capitalize}:"
     holiday_array.each do |holiday_name, supply_array|
